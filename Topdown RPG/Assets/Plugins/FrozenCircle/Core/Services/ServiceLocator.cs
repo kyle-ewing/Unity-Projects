3d525faf1c0988310b28b98bb23dd0c646bb5818
DevFrostyBeard <jpinkers@gmail.com>
2023-06-27 15:45:31 -0700

    Add initial service locator structure and utilities
    Added IService interface, ServiceLocator and TypeUtilities classes under new FrozenCircle plugin. These provide a boilerplate for ServiceLocator pattern that allows different parts of an application to be loosely coupled yet able to locate and use each other. IService interface ensures that all services will be Disposible while ServiceLocator provides mechanisms to register, unregister and get services. TypeUtilities will aid in creating instances and identifying types that extend certain interfaces.

---
+++ Topdown RPG/Assets/Plugins/FrozenCircle/Core/Services/ServiceLocator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FrozenCircle.Core.Utils;
+using UnityEngine;
+
+namespace FrozenCircle.Core.Services
+{
+    public static class ServiceLocator
+    {
+        private static Dictionary<Type, IService> services = new Dictionary<Type, IService>();
+
+        [RuntimeInitializeOnLoadMethod]
+        private static async void InitializeServiceLocator()
+        {
+            Debug.Log("[Service Locator] Initializing");
+            Application.quitting += ApplicationOnQuitting;
+
+            var serviceList = TypeUtilities.GetConcreteTypesThatExtend<IService>()
+                .Where(x => typeof(IInitializable).IsAssignableFrom(x)).ToList();
+            Debug.Log(services.Count);
+            serviceList.ForEach(async x =>
+            {
+                if (!typeof(MonoBehaviour).IsAssignableFrom(x))
+                {
+                    IService service = TypeUtilities.CreateInstance<IService>(x);
+                    if (service is IInitializable initializable)
+                    {
+                        try
+                        {
+                            await initializable.Initialize();
+                            RegisterService(service);
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogException(e);
+                        }
+                    }
+                }
+            });
+        }
+
+
+        public static void RegisterService<T>(T service)
+            where T : IService
+        {
+            if (services.ContainsKey(typeof(T)))
+            {
+                throw new Exception($"Service of type {typeof(T)} already registered");
+            }
+
+            Debug.Log($"Adding type of: {service.GetType()}");
+            services.Add(service.GetType(), service);
+        }
+
+        public static void UnregisterService<T>()
+            where T : IService
+        {
+            Type type = typeof(T);
+            if (services.ContainsKey(type) && services[type] != null)
+            {
+                services[type].Dispose();
+                services.Remove(type);
+            }
+        }
+
+        public static T GetService<T>()
+            where T : IService
+        {
+            if (services.ContainsKey(typeof(T)))
+            {
+                return (T)services[typeof(T)];
+            }
+            else
+            {
+                throw new Exception($"Service of type {typeof(T)} not registered");
+            }
+        }
+
+        private static void ApplicationOnQuitting()
+        {
+            Application.quitting -= ApplicationOnQuitting;
+            foreach (var keyValuePair in services)
+            {
+                if (keyValuePair.Value != null)
+                {
+                    keyValuePair.Value.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Topdown RPG/Assets/Plugins/FrozenCircle/Core/Utils/TypeUtilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FrozenCircle.Core.Utils
+{
+    public static class TypeUtilities
+    {
+        public static List<Type> GetConcreteTypesThatExtend<T>()
+        {
+            var baseType = typeof(T);
+            var assemblies = 
+                AppDomain
+                    .CurrentDomain
+                    .GetAssemblies()
+                    .SelectMany(x => x.GetTypes())
+                    .Where(x => !x.IsInterface && !x.IsAbstract && baseType.IsAssignableFrom(x))
+                    .ToList();
+            return assemblies;
+        } 
+        
+        public static List<Type> GetConcreteTypesThatExtend(Type baseType)
+        {
+            var assemblies = 
+                AppDomain
+                    .CurrentDomain
+                    .GetAssemblies()
+                    .SelectMany(x => x.GetTypes())
+                    .Where(x => !x.IsInterface && !x.IsAbstract && baseType.IsAssignableFrom(x))
+                    .ToList();
+            return assemblies;
+        }
+        
+        
+        public static IFinalType CreateInstance<IFinalType>(Type type)
+        {
+            if (typeof(MonoBehaviour).IsAssignableFrom(type))
+            {
+                throw new Exception("Cannot create a MonoBehaviour instance with this method.");
+            }
+            var instance = Activator.CreateInstance(type);
+            return (IFinalType) instance;
+        }
+    }
+}
